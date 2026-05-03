d19d40888a1109d438dcf6e5ce3cf0af21aa5411
Amr Bassal <amrbassal98@gmail.com>
2026-05-03 18:32:38 +0300

    implement Flask server with emotionDetector route and UI rendering

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
+
+app = Flask('Emotion Detector')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/emotionDetector')
+def detect_emotion():
+    text_to_analyze = request.args.get('textToAnalyze')
+    emotion_scores = emotion_detector(text_to_analyze)
+    anger = emotion_scores['anger']
+    disgust = emotion_scores['disgust']
+    fear = emotion_scores['fear']
+    joy = emotion_scores['joy']
+    sadness = emotion_scores['sadness']
+    dominant_emotion = emotion_scores['dominant_emotion']
+    return (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
